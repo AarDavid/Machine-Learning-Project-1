a6194a2ec133be35035e0a3c086b50aa672b1557
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2020-03-05 14:51:27 +0100

    Add files via upload

---
+++ preprocessing_1.R
@@ -0,0 +1,26 @@
+library(caret)
+library(dplyr)
+
+setwd("C:/IT/Machine Learning Course/Diamonds")
+f = "diamonds_whole_pp_1.csv"
+colClasses = c("numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", 
+               "numeric", "numeric", "numeric")
+df <- read.csv(file = f, colClasses = colClasses, sep = ",")
+
+skip <- select(df, "price")
+
+transformation =
+  preProcess(
+    df,
+    #method = c("BoxCox","center","scale", "pca")
+    method = c("BoxCox","center","scale")
+  )
+
+transformedPredictors <- predict(transformation, df)
+
+transformedPredictors["price"] <- skip["price"]
+transformedPredictors
+
+write.csv(transformedPredictors, "diamonds_whole_pp_2.csv", row.names = FALSE)
+
+
---
+++ preprocessing_2_center.R
@@ -0,0 +1,33 @@
+library(caret)
+library(dplyr)
+
+setwd("C:/IT/Machine Learning Course/Diamonds")
+f = "diamonds_val_pp_1.csv"
+colClasses = c("numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", 
+               "numeric", "numeric", "numeric")
+df <- read.csv(file = f, colClasses = colClasses, sep = ",")
+
+keep <- select(df, "price")
+drops <- c("price")
+df_drop <- df[ , !(names(df) %in% drops)]
+df_drop
+
+transformation =
+  preProcess(
+    df_drop,
+    method = c("BoxCox","center","scale", "pca")
+    #method = c("BoxCox","center","scale")
+    #method = c("BoxCox","center")
+    
+  )
+
+transformedPredictors <- predict(transformation, df_drop)
+transformedPredictors
+
+transformedPredictors
+
+test <- transformedPredictors
+transformedPredictors["price"] <- keep["price"]
+transformedPredictors
+
+write.csv(transformedPredictors, "diamonds_val_pp_2_pca.csv", row.names = FALSE)
